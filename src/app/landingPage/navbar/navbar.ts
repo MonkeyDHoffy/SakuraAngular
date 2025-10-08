bf86dc9995970aef303556ecf559e0c98b61dbd2
MonkeyDHoffy <jonekstoff@googlemail.com>
2025-10-08 23:35:12 +0200

    navbar component

--- src/app/landingPage/navbar/navbar.ts
+++ src/app/landingPage/navbar/navbar.ts
@@ -1,23 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-@Component({
-  selector: 'app-landing-page',
-  standalone: true,
-  imports: [CommonModule],
-  template: `
-    <section>
-      <h1 class="font-raleway">SAKURA RAMEN</h1>
-      <h2 class="font-raleway">Best Ramen in Town</h2>
-    </section>
-  `,
-  styleUrls: ['./landingPage.component.scss']
-})
-export class LandingPageComponent { }
+// @Component({
+//   selector: 'app-navbar',
+//   imports: [],
+//   templateUrl: './navbar.html',
+//   styleUrl: './navbar.scss'
+// })
+// export class Navbar {
 
+// }
 
 @Component({
-  selector: 'sakura-navbar',
+  selector: 'app-navbar',
   standalone: true,
   imports: [CommonModule],
   template: `
