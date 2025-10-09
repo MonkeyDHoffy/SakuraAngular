231b34b283520aeadb6bfa532918ddcd78399a6f
MonkeyDHoffy <jonekstoff@googlemail.com>
2025-10-09 20:53:05 +0200

    howtoorder style

--- src/app/main-content/landingPage/landingPage.component.ts
+++ src/app/main-content/landingPage/landingPage.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from "./navbar/navbar";
+import { HowToOrder } from '../how-to-order/how-to-order';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule, NavbarComponent],
+  imports: [CommonModule, NavbarComponent, HowToOrder],
   template: `
     <section>
       <div><app-navbar></app-navbar></div>
       <h1 class="font-raleway">SAKURA RAMEN</h1>
       <h2 class="font-raleway">Best Ramen in Town</h2>
     </section>
+    <app-how-to-order></app-how-to-order>
   `,
   styleUrls: ['./landingPage.component.scss']
 })
